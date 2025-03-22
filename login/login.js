117dbaa94b338ba6fd343d9db51fee8e733a324a
826yong <wttggjjppdd@gmail.com>
2025-03-22 23:16:38 +0800

    modify login.html

--- login/login.js
+++ login/login.js
@@ -1,25 +1,52 @@
 let captcha = "";
 
+function genNewCaptcha() {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    if (!canvas || !ctx) {
+        console.error("Canvas not found!");
+        return;
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "lightblue";
+    ctx.fillRect(20, 20, canvas.width, canvas.height);
+
+    captcha = Math.random().toString(36).substring(2, 6).toUpperCase();
+
+    ctx.font = "24px Arial";
+    ctx.fillStyle = "black";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+
+    const x = canvas.width / 2;
+    const y = canvas.height / 2 + 5;
+    ctx.fillText(captcha, x, y);
+
+    console.log("新驗證碼:", captcha);
+    
+}
+
+window.onload = genNewCaptcha;
+
 function logIn() {
     let account = document.getElementById("Account").value;
     let password = document.getElementById("Password").value;
     let verify = document.getElementById("Captcha").value.toUpperCase();
 
-    if (!(account == "admin") || !(password =="1234")) {
+    if ((account != "admin") || (password !="1234")) {
         window.alert("帳號、密碼錯誤");
+        return;
     }
 
     if (verify != captcha) {
         window.alert("驗證碼錯誤");
         genNewCaptcha();
+        return;
     }
 
     window.location.href = "https://826yong.github.io/Probability/CrazySpin/JS_version/index.html";
-
-}
-
-function genNewCaptcha() {
-    const canvas = document.getElementById("myCanvas");
-    const ctx = canvas.getContext("2d");
 
 }
